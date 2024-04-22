87e1fd5e5c42fa9baeccbdf12a0f8d65a5047686
kabeer11000 <kabeer@kabeersnetwork.tk>
2024-04-23 01:23:59 +0500

    Added, Homepage,

---
+++ src/pages/nbr/index.tsx
@@ -0,0 +1,9 @@
+import HeroSection1 from "@/components/hero-section-1/hero-section-1";
+
+export default function NarkinsBoutiqueResidency() {
+    return (
+        <div>
+            <HeroSection1/>
+        </div>
+    )
+}
