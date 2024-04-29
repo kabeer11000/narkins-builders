8975f87a32d3cb1bfa1282c3d5c8b7eb55d50962
kabeer11000 <kabeer@kabeersnetwork.tk>
2024-04-30 01:16:01 +0500

    fixed added blogs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -394,20 +394,8 @@ export default function Home() {
             "\n            .bd-placeholder-img {\n            font-size: 1.125rem;\n            text-anchor: middle;\n            -webkit-user-select: none;\n            -moz-user-select: none;\n            user-select: none;\n            }\n            @media (min-width: 768px) {\n            .bd-placeholder-img-lg {\n            font-size: 3.5rem;\n            }\n            }\n            .b-example-divider {\n            width: 100%;\n            height: 3rem;\n            background-color: rgba(0, 0, 0, .1);\n            border: solid rgba(0, 0, 0, .15);\n            border-width: 1px 0;\n            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);\n            }\n            .b-example-vr {\n            flex-shrink: 0;\n            width: 1.5rem;\n            height: 100vh;\n            }\n            .bi {\n            vertical-align: -.125em;\n            fill: currentColor;\n            }\n            .nav-scroller {\n            position: relative;\n            z-index: 2;\n            height: 2.75rem;\n            overflow-y: hidden;\n            }\n            .nav-scroller .nav {\n            display: flex;\n            flex-wrap: nowrap;\n            padding-bottom: 1rem;\n            margin-top: -1px;\n            overflow-x: auto;\n            text-align: center;\n            white-space: nowrap;\n            -webkit-overflow-scrolling: touch;\n            }\n            .btn-bd-primary {\n            --bd-violet-bg: #712cf9;\n            --bd-violet-rgb: 112.520718, 44.062154, 249.437846;\n            --bs-btn-font-weight: 600;\n            --bs-btn-color: var(--bs-white);\n            --bs-btn-bg: var(--bd-violet-bg);\n            --bs-btn-border-color: var(--bd-violet-bg);\n            --bs-btn-hover-color: var(--bs-white);\n            --bs-btn-hover-bg: #6528e0;\n            --bs-btn-hover-border-color: #6528e0;\n            --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);\n            --bs-btn-active-color: var(--bs-btn-hover-color);\n            --bs-btn-active-bg: #5a23c8;\n            --bs-btn-active-border-color: #5a23c8;\n            }\n            .bd-mode-toggle {\n            z-index: 1500;\n            }\n            .bd-mode-toggle .dropdown-menu .active .bi {\n            display: block !important;\n            }\n        "
         }}
       />
-      <BlogsSection/>
+      <BlogsSection />
       <Footer />
-      <script dangerouslySetInnerHTML={{
-        __html: `window.onclick = function (e) {
-                e.preventDefault();
-                if (e.target.localName == 'a') {
-                    // https://narkinsbuilders.com/
-                  window.parent.postMessage({target: e.target.getAttribute('href'), type: "loc"}, 'http://localhost:3000/'); // Change URL to your parent frame's domain
-                }
-            }
-            window.onload = () => {
-                  window.parent.postMessage({px: document.body.clientHeight, type: "height"}, 'http://localhost:3000/'); // Change URL to your parent frame's domain
-                //   alert("a")
-            }`}} />
     </>
   )
 }
