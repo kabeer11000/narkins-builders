1cb6a6827d94a1a24f12cb61e4796ddf5394e48a
kabeer11000 <kabeer@kabeersnetwork.tk>
2024-05-03 02:04:18 +0500

    switched runtimes

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -49,7 +49,7 @@ export default function VideoPlayer({ src, poster }: { src: string, poster?: str
                     onPlayPause={togglePlayPause}
                 />
             </div>
-            <video className="w-full" poster={poster} ref={videoRef} preload="yes" playsInline loop muted autoPlay>
+            <video className="w-full" crossOrigin="anonymous" poster={poster} ref={videoRef} preload="yes" playsInline loop muted autoPlay>
                 <source src={src} type="video/mp4"/>
             </video>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -128,7 +128,7 @@ export default function Home() {
             </div>
           </div>
           <video
-            preload="yes" playsInline loop muted autoPlay
+            preload="yes" playsInline loop muted autoPlay crossOrigin="anonymous"
             className="absolute z-1 w-auto min-w-full min-h-full filter brightness-[50%] max-w-none"
           >
             <source
